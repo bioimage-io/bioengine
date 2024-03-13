3efcd274d2fb2ab943aae03a9963c56875dae379
Nanguage <Nanguage@yahoo.com>
2024-03-13 10:30:30 +0100

    change minio root user

--- bioimageio/engine/__main__.py
+++ bioimageio/engine/__main__.py
@@ -13,7 +13,7 @@ async def start_server(host = "0.0.0.0", port = 9000, public_base_url = ""):
     # get current file path so we can get the path of apps under the same directory
     # current_dir = os.path.dirname(os.path.abspath(__file__))
     launcher = HyphaLauncher()
-    minio_root_user = secrets.token_urlsafe(16)
+    minio_root_user = "minio_root_user"
     minio_root_password = secrets.token_urlsafe(16)
     info = await launcher.launch_s3_server(minio_root_user, minio_root_password)
     logger.info("S3 server launched at: %s", info["port"])
